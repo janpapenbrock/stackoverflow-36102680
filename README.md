f62faccebbe58c0afe7a7940b830ef62cb326165
janpapenbrock <lunoxx@gmail.com>
2016-03-23 11:52:42 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # stackoverflow-36102680
+
+
+##  Start
+    
+    docker-compose up
+    
+## Open
+
+http://localhost:4400/Scripts/prodlist.asp?strSearch=REMS+POWER+PRESS+E+ATTREZZATURE&strSearchType=OR&strSearchMin=0&strSearchMax=0&strSearchCat=0&curPage=86&sortField=sku
---
+++ public/index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo "Hello, root directory here.";
+
+?><p><a href="http://localhost:4400/Scripts/prodlist.asp?strSearch=REMS+POWER+PRESS+E+ATTREZZATURE&strSearchType=OR&strSearchMin=0&strSearchMax=0&strSearchCat=0&curPage=86&sortField=sku">Hey, I am redirecting to root.</a></p>
+<p><a href="http://localhost:4400/Scripts/prodlist.asp?strSearch=REMS+POWER+PRESS+E+ATTREZZATURE&strSearchType=OR&strSearchMin=0&strSearchMax=0&strSearchCat=0&curPage=86&sortField=sku2">Hey, I am redirecting to root.</a></p>
